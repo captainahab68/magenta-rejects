fed3059bf608ccea67edab95300f271e234ac8ba
Alex Miller <ajm9594@gmail.com>
2021-02-21 14:47:40 -0800

    first draft nav

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './home.css'
 import Logo from '../images/MRlogo-01.svg'
+import {Link} from 'react-router-dom'
 class Home extends React.Component {
     render() {
       return (
+          <>
           <header>
               <img id="logo" src={Logo} alt="Magenta Rejects Logo"/>
               <div className="titlebox">
@@ -11,6 +13,15 @@ class Home extends React.Component {
                   <h2>...is an art collective</h2>
               </div>
           </header>
+          <nav>
+            <Link className="link">rebecca</Link>
+            <Link className="link">marion</Link>
+            <Link className="link">lux pacifica</Link>
+            <Link className="link">s.a. ribble</Link>
+            <Link className="link">collabs</Link>
+            <Link className="link">blog</Link>
+          </nav>
+          </>
     )  
     }
 }

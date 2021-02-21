92094ff3f9606a399f01ce233311a7e0df782643
Alex Miller <ajm9594@gmail.com>
2021-02-21 14:27:57 -0800

    home page header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home'
 function App() {
+  
   return (
-    <div>
+    <>
       <Switch>
         <Route path='/' component={ Home }/>
       </Switch>
-    </div>
+    </>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import './home.css'
+import Logo from '../images/MRlogo-01.svg'
 class Home extends React.Component {
     render() {
       return (
-        <h1>Magenta Rejects</h1>
+          <header>
+              <img id="logo" src={Logo} alt="Magenta Rejects Logo"/>
+              <div className="titlebox">
+                  <h1>Magenta Rejects</h1>
+                  <h2>...is an art collective</h2>
+              </div>
+          </header>
     )  
     }
 }
